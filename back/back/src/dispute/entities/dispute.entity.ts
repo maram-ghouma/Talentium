f27c6e0ced5a9dc74d9e0ed57ba6f083bb6aafb9
mahjoubr <152096395+mahjoubr@users.noreply.github.com>
2025-06-04 14:32:51 +0100

    added the payment service logic and fixed module confog

--- back/back/src/dispute/entities/dispute.entity.ts
+++ back/back/src/dispute/entities/dispute.entity.ts
@@ -1,6 +1,10 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
 import { Mission } from "src/mission/entities/mission.entity";
 import { User } from "src/user/entities/user.entity";
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { registerEnumType } from '@nestjs/graphql';
+
+
 
 export enum DisputeStatus {
   OPEN = 'OPEN',
@@ -8,27 +12,39 @@ export enum DisputeStatus {
   RESOLVED = 'RESOLVED',
   REJECTED = 'REJECTED',
 }
+registerEnumType(DisputeStatus, {
+  name: 'DisputeStatus', 
+  description: 'The status of a dispute',
+});
 
+@ObjectType()
 @Entity()
 export class Dispute {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(() => Mission)
   @ManyToOne(() => Mission, mission => mission.disputes)
   mission: Mission;
 
+  @Field(() => User)
   @ManyToOne(() => User)
   openedBy: User;
 
+  @Field(() => String)
   @Column()
   reason: string;
 
+  @Field(() => DisputeStatus)
   @Column({ type: 'enum', enum: DisputeStatus, default: DisputeStatus.OPEN })
   status: DisputeStatus;
 
+  @Field(() => Date)
   @CreateDateColumn()
   openedAt: Date;
 
+  @Field(() => String, { nullable: true })
   @Column({ nullable: true })
   resolution: string;
 }
