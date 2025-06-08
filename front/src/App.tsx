097e4148a7effc24b861f5b3a3f7f852ea76b4a3
Maher-Wali <walimaher00@gmail.com>
2025-06-08 19:35:10 +0100

    fixed kanban auth and conversation creation when application is accepted

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -47,6 +47,7 @@ import ReviewPage from './Pages/Review/ReviewPage';
 import NotificationContainer from './components/realtime_notification/NotificationContainer';
 import { useNotifications } from './hooks/useNotifications';
 import FreelancerKanban from './Pages/Freelancer/FreelancerKanban';
+import Kanban from './Pages/kanban'
 
 function App() {
   return (
@@ -78,7 +79,7 @@ function App() {
   <Route path="/client/profile" element ={<ClientProfilePage/>}/>
   <Route path="/client/profile/:id" element ={<ClientProfilePage/>}/>
   <Route path="/client/chat" element={<ClientChat />} />
-  <Route path="/kanban/:id" element={<ClientKanban />} />
+  <Route path="/kanban/:id" element={<Kanban />} />
   <Route path="/jalons" element ={<JalonsPage/>}/>
 
   <Route path="/signup" element={<SignUpPage />} />
